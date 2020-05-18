ebd1367cf63b5bf1a35f9dcb5ca685cee850e78f
anishasnet <anishasnet@gmail.com>
2020-05-17 20:09:11 -0700

    html template and generated and functioning order for inquirer prompts

--- index.js
+++ index.js
@@ -1,89 +1,144 @@
-const inquirer = require('inquirer')
-const employeeTypes = ['employee', 'manager', 'engineer', 'intern']
-const Employee = require('./lib/employee.js')
-const questions = () => {
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHtml = require('./src/generateHtml.js');
+const employeeTypes = ['employee', 'manager', 'engineer', 'intern'];
+const Employee = require('./lib/employee.js');
+const Engineer = require('./lib/engineer.js');
+const Intern = require('./lib/intern.js');
+const Manager = require('./lib/manager.js');
+
+
+let employeeData = [];
+const managerQuestions = () => {
     return inquirer.prompt([
         {
             type: 'text',
-            name: 'employeeType',
-            message: 'What type of employee:',
-            validate: employeeTypeInput => {
-                if (employeeTypeInput) {
-                    if (employeeTypes.includes(employeeTypeInput.toLowerCase())){
-                        return true;
-                    } else {
-                        console.log("\nEmployee type must be either Employee, Manager, Engineer, or Intern")
-                    }
-                } else {
-                    console.log("Please enter the employee type")
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'text',
-            name: 'name',
-            message: 'Name:',
+            name: 'managerName',
+            message: 'What is the team manager\'s name? ',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
                 } else {
-                    console.log("Please enter the employee's name")
+                    console.log("Please enter the manager's name.")
                     return false;
                 }
             }
         },
         {
             type: 'text',
-            name: 'id',
-            message: 'Employee id:',
+            name: 'managerId',
+            message: 'What is the team manager\'s id? ',
             validate: idInput => {
                 if (idInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the employee's id")
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'text',
-            name: 'email',
-            message: 'Email:',
-            validate: emailInput => {
-                if (emailInput) {
-                    if (emailInput.includes('@')){
+                    if (parseInt(idInput) == idInput) {
                         return true;
                     } else {
-                        console.log("\nPlease enter a valid email");
+                        console.log('\nEmployee id\'s can only have numbers.')
                         return false;
                     }
                 } else {
-                    console.log("Please enter the employee's email")
+                    console.log("Please enter the manager's id.")
                     return false;
                 }
             }
         },
         {
             type: 'text',
-            name: 'officeNumber',
-            message: 'Office Number:',
+            name: 'managerEmail',
+            message: 'What is the team manager\'s email? ',
+            validate: emailInput => {
+                if (emailInput) {
+                    if (emailInput.includes('@')) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email.");
+                        return false;
+                    }
+                    return true;
+                } else {
+                    console.log("Please enter the manager's email.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'managerOfficeNumber',
+            message: 'What is the team manager\'s office number? ',
             validate: officeNumberInput => {
                 if (officeNumberInput) {
                     return true;
                 } else {
-                    console.log("Please enter the employee's office number")
+                    console.log("Please enter the manager's office number.")
                     return false;
                 }
-            },
-            when: function( answers ) {
-                return answers.employeeType.toLowerCase() === "manager";
-              },
+            }
+        },
+        {
+            type: 'list',
+            name: 'nextEmployee',
+            message: 'Which type of team member would you like to add? ',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        },
+    ])
+}
+
+const engineerQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'engineerName',
+            message: 'What is the engineer\'s name? ',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's name.");
+                    return false;
+                }
+            }
         },
         {
             type: 'text',
-            name: 'github',
-            message: 'Github username:',
+            name: 'engineerId',
+            message: 'What is the engineer\'s id? ',
+            validate: idInput => {
+                if (idInput) {
+                    if (parseInt(idInput) == idInput) {
+                        return true;
+                    } else {
+                        console.log('\nEmployee id\'s can only have numbers.')
+                        return false;
+                    }
+                } else {
+                    console.log("Please enter the engineer's id.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'engineerEmail',
+            message: 'What is the engineer\'s email? ',
+            validate: emailInput => {
+                if (emailInput) {
+                    if (emailInput.includes('@')) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email.");
+                        return false;
+                    }
+                    return true;
+                } else {
+                    console.log("Please enter the engineer's email.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'engineerGithub',
+            message: 'What is the engineer\'s Github username? ',
             validate: githubInput => {
                 if (githubInput) {
                     return true;
@@ -91,41 +146,163 @@ const questions = () => {
                     console.log("Please enter the engineer's Github username.")
                     return false;
                 }
-            },
-            when: function( answers ) {
-                return answers.employeeType.toLowerCase() === "engineer";
-              },
+            }
         },
         {
-            type: 'text',
-            name: 'school',
-            message: 'School:',
-            validate: schoolInput => {
-                if (schoolInput) {
-                    return true;
-                } else {
-                    console.log("Please enter the intern's school")
-                    return false;
-                }
-            },
-            when: function( answers ) {
-                return answers.employeeType.toLowerCase() === "intern";
-              },
+            type: 'list',
+            name: 'nextEmployee',
+            message: 'Which type of team member would you like to add? ',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
         },
-        {
-            type: 'confirm',
-            name: 'newEmployee',
-            message: 'Would you like to input another employee: ',
-        },
-
-
     ])
 }
 
-questions().then(answers => {
-    if (answers.newEmployee) {
-        return questions();
+
+const internQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'internName',
+            message: 'What is the intern\'s name? ',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'internId',
+            message: 'What is the intern\'s id? ',
+            validate: idInput => {
+                if (idInput) {
+                    if (parseInt(idInput) == idInput) {
+                        return true;
+                    } else {
+                        console.log('\nEmployee id\'s can only have numbers.')
+                        return false;
+                    }
+                } else {
+                    console.log("Please enter the intern's id.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'internEmail',
+            message: 'What is the intern\'s email? ',
+            validate: emailInput => {
+                if (emailInput) {
+                    if (emailInput.includes('@')) {
+                        return true;
+                    } else {
+                        console.log("\nPlease enter a valid email.");
+                        return false;
+                    }
+                    return true;
+                } else {
+                    console.log("Please enter the intern's email.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'internSchool',
+            message: 'What is the intern\'s school? ',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the intern's school.")
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'nextEmployee',
+            message: 'Which type of team member would you like to add? ',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        },
+    ])
+}
+
+
+const engineerThenStatement = () => {
+    engineerQuestions().then(answers => {
+        let engineerObj = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+        engineerObj.role = engineerObj.getRole()
+        employeeData.push(engineerObj);
+        if (answers.nextEmployee == "Engineer") {
+            return engineerThenStatement();
+        } else if (answers.nextEmployee == "Intern") {
+            return internThenStatement();
+        } else {
+            fs.writeFile('./dist/index.html', generateHtml(employeeData), err => {
+                if (err) throw err;
+                console.log('Portfolio complete! Check out index.html to see the output!');
+                
+                fs.copyFile('./src/style.css', './dist/style.css', err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('Style sheet copied successfully!');
+                    });
+            })
+        }
+    })
+}
+
+const internThenStatement = () => {
+    internQuestions().then(answers => {
+        let internObj = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+        internObj.role = internObj.getRole()
+        employeeData.push(internObj);
+        if (answers.nextEmployee == "Engineer") {
+            return engineerThenStatement();
+        } else if (answers.nextEmployee == "Intern") {
+            return internThenStatement();
+        } else {
+            fs.writeFile('./dist/index.html', generateHtml(employeeData), err => {
+                if (err) throw err;
+                console.log('Portfolio complete! Check out index.html to see the output!');
+                
+                fs.copyFile('./src/style.css', './dist/style.css', err => {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log('Style sheet copied successfully!');
+                    });
+            })
+        }
+    })
+}
+managerQuestions().then(answers => {
+    let managerObj = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+    managerObj.role = managerObj.getRole()
+    employeeData.push(managerObj);
+    if (answers.nextEmployee == "Engineer") {
+        return engineerThenStatement();
+    } else if (answers.nextEmployee == "Intern") {
+        return internThenStatement();
     } else {
-        console.log(answers);
+        fs.writeFile('./dist/index.html', generateHtml(employeeData), err => {
+            if (err) throw err;
+            console.log('Portfolio complete! Check out index.html to see the output!');
+            
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Style sheet copied successfully!');
+                });
+        })
     }
 })
---
+++ src/generateHtml.js
@@ -0,0 +1,79 @@
+const generateHtml = employeeData => {
+    const employeeDataArr = employeeData.map(obj => {
+        if (obj.role.toLowerCase() == 'manager'){
+        return `
+
+<div class = "card">
+    <div class = "card-header">
+        <h3>${obj.name}</h3>
+        <i class="fas fa-mug-hot"><h3 class = "role">${obj.role}</h3></i>
+    </div>
+    <p>Id: ${obj.id}</p>
+    <p>Email: <span><a href = "mailto:${obj.email}">${obj.email}</a></span></p>
+    <p>Office Number: ${obj.officeNumber}</p>
+</div>
+
+
+          `;} else if (obj.role.toLowerCase() == 'engineer') {
+            return `
+
+<div class = "card">
+    <div class = "card-header">
+        <h3>${obj.name}</h3>
+        <i class="fas fa-glasses"><h3 class = "role">${obj.role}</h3></i>
+    </div>
+    <p>Id: ${obj.id}</p>
+    <p>Email: <span><a href = "mailto:${obj.email}">${obj.email}</a></span></p>
+    <p>Github Username: <span><a href = "https://www.github.com/${obj.github}">${obj.github}</a></span></p>
+</div>
+
+            `;
+          } else if (obj.role.toLowerCase() == 'intern') {
+            return `
+    
+<div class = "card">
+    <div class = "card-header">
+        <h3>${obj.name}</h3>
+        <i class="fas fa-user-graduate"><h3 class = "role">${obj.role}</h3></i>
+    </div>
+    <p>Id: ${obj.id}</p>
+    <p>Email: <span><a href = "mailto:${obj.email}">${obj.email}</a></span></p>
+    <p>School: ${obj.school}</span></p>
+</div>
+
+                        `;
+          }
+      });
+    
+    return `
+    <link
+    rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+    crossorigin="anonymous"
+  />
+<link rel="stylesheet" href="./style.css">
+<body>
+    <div class = "header-container">
+        <h1 class = "header">Team Portfolio</h1>
+    </div>
+    <div class = "container">
+        ${employeeDataArr.join('')}
+    </div>
+</body>
+    `
+}
+
+ // <div class = "container">    
+    //     <div class = "card">
+    //         <div class = "card-header">
+    //             <h3>Anshul</h3>
+    //             <i class="fas fa-mug-hot"><h3 class = "role">Manager</h3></i>
+    //         </div>
+    //         <p>Id: 31</p>
+    //         <p>Email: sinha.anshul1@gmail.com</p>
+    //         <p>Office Number: 23</p>
+    //     </div>
+    // </div>
+
+module.exports = generateHtml
